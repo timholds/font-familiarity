d7311a9d03f3d86e641a3f873f5bec861b9a2268
timholds <tholdsworth@sandiego.edu>
2025-04-03 19:00:33 -0700

    get our font images looking nice

--- data_generation/create_font_images.py
+++ data_generation/create_font_images.py
@@ -236,7 +236,7 @@ class FontRendererWorker:
     def _setup_driver(self):
         chrome_options = Options()
         chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--window-size=1920,1080')
+        chrome_options.add_argument(f'--window-size={self.image_size[0]},{self.image_size[1]}')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
