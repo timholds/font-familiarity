b22ed39471f2db77c126ac3c4f05a0b5b268ef4d
timholds <tholdsworth@sandiego.edu>
2025-04-24 17:13:54 -0700

    add patch size to model name and add arg for it in train, untested

--- ml/train.py
+++ ml/train.py
@@ -358,6 +358,7 @@ def main():
     parser.add_argument("--char_model", action="store_true", help="Use character-based model")
     parser.add_argument("--pretrained_model", type=str, default=None, help="Path to pretrained model")
     parser.add_argument("--use_precomputed_craft", action="store_true", help="Use precomputed CRAFT results")
+    parser.add_argument("--patch_size", type=int, default=32, help="Size of character patches")
     args = parser.parse_args()
     warmup_epochs = max(args.epochs // 5, 1)
 
@@ -444,7 +445,7 @@ def main():
             model = CRAFTFontClassifier(
                 num_fonts=num_classes,
                 device=device,
-                patch_size=32,
+                patch_size=args.patch_size,
                 embedding_dim=args.embedding_dim,
                 craft_fp16=use_fp16,
                 use_precomputed_craft=args.use_precomputed_craft,
@@ -665,7 +666,8 @@ def main():
                 prefix='fontCNN',
                 batch_size=args.batch_size,
                 embedding_dim=args.embedding_dim,
-                initial_channels=args.initial_channels
+                initial_channels=args.initial_channels,
+                patch_size=args.patch_size
             )
             # classifier_shape = best_model_state['model_state_dict']['classifier.weight'].shape
             # assert classifier_shape[0] == num_classes, (
