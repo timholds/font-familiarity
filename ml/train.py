ca656e688301e1af68cc55988323370fbbdf24ea
timholds <tholdsworth@sandiego.edu>
2025-05-01 01:43:49 -0700

    add gradient clipping

--- ml/train.py
+++ ml/train.py
@@ -111,6 +111,7 @@ def train_epoch(model, train_loader, criterion, optimizer, device, epoch,
         # Compute loss and backward
         loss = criterion(logits, targets)
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         
         
